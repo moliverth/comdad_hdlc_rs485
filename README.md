cffbd55d6c8cb67d1ed922368b2db760087038a4
moliverth <moliver.thiago@gmail.com>
2023-11-24 17:40:31 -0300

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,109 @@
-# comdad_hdlc_rs485
+# Trocador de frames HDLC sobre RS485
 Atividade da disciplina de Comunicação de Dados do curso de Engenharia de Controle e Automação do IFES Campus Serra.
+
+## Descrição
+Este repositório contém a implementação de um trocador de frames HDLC sobre a interface RS485, desenvolvido como parte do projeto da disciplina de Comunicação de Dados. O código foi projetado para permitir a comunicação entre um dispositivo primário e dois dispositivos secundários, utilizando o protocolo HDLC.
+
+## Funcionamento Geral
+O código do dispositivo primário e secundário é praticamente idêntico, com exceção do tratamento de mensagens após o recebimento (roteamento) e das iniciativas. A seguir, apresentamos o funcionamento geral do sistema.
+
+### Primário
+- O dispositivo primário pode enviar frames de informação (info) se não houverem enlaces estabelecidos no momento.
+- Após o envio de um frame info, o primário aguarda a resposta do dispositivo secundário em forma de frames supervisórios (super), indicando o status da mensagem.
+- Se o frame de resposta não for recebido em um determinado tempo, será feita uma nova tentativa de envio até um número de tentativas predefinido.
+- O primário pode receber a resposta com um final = 0, indicando que virá em seguida um frame info. Caso venha um frame de informação, ele será tratado, e ocorrerá a resposta em formato de frame supervisório. O secundário também realiza tentativas até uma das condições de parada.
+
+### Secundário
+- O dispositivo secundário, fora de um enlace, aguarda por frames de informação (info) do primário.
+- Após o recebimento de um frame info, o secundário responde com um frame supervisório (super), indicando se a mensagem foi recebida corretamente.
+- Caso o conteúdo da mensagem exija uma resposta, o supervisório de resposta será enviado com final = 0, e um frame info com final = 1 será enviado em seguida.
+
+## Envio de Mensagens
+- As mensagens são enviadas encapsuladas em frames de informação (info).
+- O protocolo HDLC é utilizado para delimitar os frames, com início e fim marcados pelo byte de flag (0x7E).
+- O controle do frame info contém informações sobre os números de sequência (N(s) e N(r)), indicadores de pool ou final, e controle de fluxo.
+
+## Recebimento de Mensagens
+- Tanto o primário quanto o secundário estão disponíveis para receber frames de informação (info) a qualquer momento.
+- Ao receber um frame info, o dispositivo responde obrigatoriamente com um frame supervisório (super) indicando o status da mensagem.
+
+## Frames Supervisórios
+- Os frames supervisórios (super) são utilizados para confirmar o recebimento de mensagens e indicar o status da comunicação.
+- Existem dois tipos de frames supervisórios:
+    - RR (Receive Ready): Indica que o dispositivo está pronto para receber a próxima mensagem.
+    - REJ (Reject): Indica que houve algum erro na mensagem recebida, solicitando sua retransmissão.
+
+# Execução e Explicação da Saída do Monitor Serial
+
+A seguir, segue uma explicação da saída do monitor serial para o comportamento esperado do envio de mensagens com requisição de resposta.
+
+'''
+Primary Device - Initing...
+My Address: 1
+>> send:'ok?' to 0x02
+SEND: 0x7E 0x02 0x19 0x6F 0x6B 0x3F 0x79 0xA5 0x7E n(s)=1 n(r)=1 ASCII:'ok?' P/F:1 (Info)
+RECV: 0x7E 0x01 0x81 0x81 0x01 0x7E n(r)=1 CRC:OK P/F:0 (Super - RR)
+>> recv: RR CRC:OK n(r)=1 from 0x02
+RECV: 0x7E 0x01 0x19 0x6F 0x6B 0x4B 0x06 0x7E n(s)=1 n(r)=1 ASCII:'ok' CRC:OK P/F:1 (Info)
+>> recv:'ok' CRC:OK n(s)=1 n(r)=1 from 0x02
+>> resp: RR n(r)=1 to 0x02
+SEND: 0x7E 0x02 0x81 0x83 0x01 0x7E n(r)=1 P/F:0 (Super - RR)
+'''
+
+### Comportamento Esperado - Primary Device 0x01
+1. Initing...: Indica o início da execução do dispositivo primário.
+2. My Address: 1: Mostra o endereço atribuído ao dispositivo primário (1).
+3. send:'ok?' to 0x02: Envia a mensagem 'ok?' para o dispositivo secundário com endereço 0x02.
+4. SEND: Exibe o frame de informação enviado, indicando o número de sequência (n(s)), número de recebimento (n(r)), conteúdo ASCII 5. ('ok?'), e se é um frame final (P/F).
+5. RECV: Indica a recepção de um frame supervisório (super) do dispositivo secundário, confirmando a prontidão para receber a próxima mensagem (RR).
+6. recv: RR CRC:OK n(r)=1 from 0x02: Mostra a confirmação da recepção do dispositivo secundário (0x02).
+7. RECV: Indica a recepção de um frame de informação do dispositivo secundário, mostrando o conteúdo ('ok').
+8. recv:'ok' CRC:OK n(s)=1 n(r)=1 from 0x02: Confirma a recepção da mensagem 'ok' do dispositivo secundário.
+9. resp: RR n(r)=1 to 0x02: Responde ao dispositivo secundário confirmando a prontidão para receber a próxima mensagem (RR).
+10. SEND: Envia um frame supervisório (super) para o dispositivo secundário, indicando prontidão para receber a próxima mensagem (RR).
+
+**********
+
+'''
+### Secondary Device - Initing...
+My Address: 2
+RECV: 0x7E 0x02 0x19 0x6F 0x6B 0x3F 0x79 0xA5 0x7E n(s)=1 n(r)=1 ASCII:'ok?' CRC:OK P/F:1 (Info)
+>> recv:'ok?' CRC:OK n(s)=1 n(r)=1 from 0x01
+>> resp: RR n(r)=1 to 0x01
+SEND: 0x7E 0x01 0x81 0x81 0x01 0x7E n(r)=1 P/F:0 (Super - RR)
+>> send:'ok' to Primary
+SEND: 0x7E 0x01 0x19 0x6F 0x6B 0x4B 0x06 0x7E n(s)=1 n(r)=1 ASCII:'ok' P/F:1 (Info)
+RECV: 0x7E 0x02 0x81 0x83 0x01 0x7E n(r)=1 CRC:OK P/F:0 (Super - RR)
+>> recv: RR CRC:OK n(r)=1 from 0x01
+'''
+
+### Comportamento Esperado - Secondary Device 0x02
+1. Initing...: Indica o início da execução do dispositivo secundário 1.
+2. My Address: 2: Mostra o endereço atribuído ao dispositivo secundário 1 (2).
+3. RECV: Indica a recepção de um frame de informação do dispositivo primário, mostrando o conteúdo ('ok?').
+4. recv:'ok?' CRC:OK n(s)=1 n(r)=1 from 0x01: Confirma a recepção da mensagem 'ok?' do dispositivo primário (0x01).
+5. resp: RR n(r)=1 to 0x01: Responde ao dispositivo primário confirmando a prontidão para receber a próxima mensagem (RR).
+6. SEND: Envia um frame de informação para o dispositivo primário, indicando o conteúdo ('ok') e que é um frame final (P/F).
+7. RECV: Indica a recepção de um frame supervisório (super) do dispositivo primário, confirmando a prontidão para receber a próxima mensagem (RR).
+8. recv: RR CRC:OK n(r)=1 from 0x01: Confirma a prontidão do dispositivo primário para receber a próxima mensagem.
+
+**********
+
+'''
+Secondary Device - Initing...
+My Address: 3
+RECV: 0x7E 0x02 0x19 0x6F 0x6B 0x3F 0x79 0xA5 0x7E <drop>
+RECV: 0x7E 0x01 0x81 0x81 0x01 0x7E <drop>
+RECV: 0x7E 0x01 0x19 0x6F 0x6B 0x4B 0x06 0x7E <drop>
+RECV: 0x7E 0x02 0x81 0x83 0x01 0x7E <drop>
+'''
+
+### Comportamento Esperado - Secondary Device 0x03
+1. Initing...: Indica o início da execução do dispositivo secundário 2.
+2. My Address: 3: Mostra o endereço atribuído ao dispositivo secundário 2 (3).
+3. RECV: Indica a tentativa de recepção de um frame de informação do dispositivo primário, mas o frame é descartado.
+4. RECV: Indica a tentativa de recepção de um frame supervisório (super) do dispositivo primário, mas o frame é descartado.
+5. RECV: Indica a tentativa de recepção de um frame de informação do dispositivo primário, mas o frame é descartado.
+6. RECV: Indica a tentativa de recepção de um frame supervisório (super) do dispositivo primário, mas o frame é descartado.
+
+> Nota: Este README fornece uma visão geral do projeto. Consulte a documentação interna do código-fonte para informações detalhadas sobre a implementação.
---
+++ primary_01/primary_01.ino
@@ -0,0 +1,454 @@
+// Primary
+
+#include "CRC16.h"
+
+#define A_PIN 5
+#define B_PIN 6
+
+#define MAX_PAYLOAD_SIZE 58 // in bytes
+#define MAX_FRAME_LENGTH 64 // MAX_PAYLOAD_SIZE + 6 (Address + Control + 2*CRC + 2*Flags)
+
+#define DEBUG_LEVEL 1 // 1 show full frames, 2 show full frames + received byte
+
+#define LOCAL_ADDRESS 0x01
+// ***** reserved addresses: *****
+//         0x00 (0)   - Default (route to Primary device)
+//         0xff (255) - Broadcast (unimplemented)
+
+// ##### FRAME #####
+uint16_t send_seq_num;  // shall be incremented each sucessive information frame sent, reset in link inicialization
+uint16_t recv_seq_num;  // represents next expected information frame in sequence
+char payload_[MAX_PAYLOAD_SIZE]; // message to sent 
+uint16_t auto_payload_size;
+
+uint8_t linked_device; // address
+
+CRC16 crc; // error check
+
+// ##### TEMPLATES #####
+uint8_t flag =  0b01111110; // 0x7e
+uint8_t info =  0b00000000; // 0[7],    N(s)[6:4], (P/F)[3], N(r)[2:0]
+uint8_t super = 0b10000000; // 1[7], 0[6], S[5:4], (P/F)[3], N(r)[2:0]
+uint8_t unnum = 0b11000000; // 1[7], 1[6], M[5:4], (P/F)[3], M[2:0]
+
+// ### SETS ###
+struct bytedata {bool is_refreshed; uint8_t value;};
+struct framedata {bool ready; uint16_t lenght; uint8_t buffer[MAX_FRAME_LENGTH];};
+
+bytedata  received_byte;
+framedata frame;
+
+// ******************************************************************
+
+// ##########################
+// #####      RS485     #####
+// ##########################
+void sendByte(uint8_t byte_, bool set_receive = false){
+  // start bit: b'0 (A > B)
+  pinMode(A_PIN, OUTPUT); pinMode(B_PIN, OUTPUT); // set hardware send mode
+  digitalWrite(A_PIN, 255); digitalWrite(B_PIN, 0); // b'0
+  delay(5); // 200hz bit delay
+  
+  // content: 8 bits
+  for (uint8_t bit_pos = 0; bit_pos < 8; bit_pos++) {
+    digitalWrite(A_PIN, not getBit(byte_, (7-bit_pos)));
+    digitalWrite(B_PIN,     getBit(byte_, (7-bit_pos)));
+    delay(5); // 200hz bit delay
+  }  
+  
+  // final bit: b'1 (B > A)
+  digitalWrite(A_PIN, 0); digitalWrite(B_PIN, 255); // b'1
+  delay(5); // 200hz bit delay
+  if (set_receive){
+    pinMode(A_PIN, INPUT); pinMode(B_PIN, INPUT_PULLUP); // set hardware receive mode
+  }
+  if (DEBUG_LEVEL > 0) {
+    Serial.print(byte_ > 0xf ? " 0x" : " 0x0"); 
+    Serial.print(byte_, HEX);
+  }
+}
+
+bytedata scanByte(uint8_t timeout_ms = 100, uint16_t scan_delay_us = 100){
+  // return false and 0 if not receive start bit within the timeout
+  uint8_t byte_ = 0; // alocate received byte
+
+  unsigned long init_time = millis();
+  while (init_time + timeout_ms > millis()){
+    if (digitalRead(A_PIN) && not digitalRead(B_PIN)){ // start bit: b'0 (A > B)
+      delay(6); // wait for MSB set
+      
+      if (DEBUG_LEVEL > 1) Serial.print("\nBYTE RECV: (MSB)"); 
+      
+      for (int bit_pos = 0; bit_pos < 8; bit_pos++) { // sequentially records the expected 8 bits
+        if (not digitalRead(A_PIN) && digitalRead(B_PIN)){ // if A=0 and B=1 b'1 otherwise b'0
+          bitSet(byte_, (7-bit_pos)); // MSB first
+        }
+        if (DEBUG_LEVEL > 1) Serial.print(getBit(byte_, (7-bit_pos))); // print received bit
+        delay(5); // 200hz bit delay
+      } 
+      if (DEBUG_LEVEL > 1) {
+        Serial.print(byte_ > 0xf ? " 0x" : " 0x0"); 
+        Serial.print(byte_, HEX);
+      }
+      received_byte.is_refreshed = true;
+      received_byte.value = byte_;
+      return received_byte;
+    }
+    delayMicroseconds(scan_delay_us);
+  }
+  bool byte_is_refreshed = false;
+  received_byte.is_refreshed = false;
+  received_byte.value = 0;
+  return received_byte;
+}
+
+// ******************************************************************
+
+
+// ##########################
+// #####      HDCL      #####
+// ##########################
+framedata addToFrameBuffer(uint8_t byte_){
+  if (frame.ready) return frame;
+  if (not frame.lenght and byte_ != flag) return frame;  // drop first byte except flag
+
+  frame.buffer[frame.lenght] = byte_;
+  frame.lenght++;
+  frame.ready = (frame.lenght > 1 and byte_ == flag) or (frame.lenght >= MAX_FRAME_LENGTH);
+}
+
+void resetFrameBuffer(){
+  frame.ready = false;
+  frame.lenght = 0;
+  memset(frame.buffer, 0, MAX_FRAME_LENGTH * sizeof(uint8_t));
+}
+
+void sendFlag(bool final_flag = false){
+  crc.restart();
+  sendByte(flag, final_flag);
+}
+
+void sendAddress(uint8_t address){
+  linked_device = address;
+  sendByte(address);
+  crc.add(address);
+}
+
+void sendControl(uint8_t control, uint8_t sqn, bool pool_or_final, uint8_t rqn, uint8_t super_command = 0b00){
+  // control input is a template
+  // sqn = send sequence number, rqn = receive sequence number
+  // pool mean the Primary is requesting a response, consenting to destine permission to use the link 
+  // final mean the Secondary finished your queue, and abdicate the link permission
+  
+  if (control == info){
+    if (getBit(rqn, 0)) bitSet(control, 0);
+    if (getBit(rqn, 1)) bitSet(control, 1);
+    if (getBit(rqn, 2)) bitSet(control, 2);
+    if (pool_or_final)  bitSet(control, 3);
+    if (getBit(sqn, 0)) bitSet(control, 4);
+    if (getBit(sqn, 1)) bitSet(control, 5);
+    if (getBit(sqn, 2)) bitSet(control, 6);
+  }
+
+  if (control == super){
+    // super_command : 0b00-RR(Receive ready); 0b01-REJ(Reject); 0b10-RNR(Receive not ready); 0b11-SREJ(Selective reject)
+    if (getBit(rqn, 0)) bitSet(control, 0);
+    if (getBit(rqn, 1)) bitSet(control, 1);
+    if (getBit(rqn, 2)) bitSet(control, 2);
+    if (pool_or_final)  bitSet(control, 3);
+    if (getBit(super_command, 0)) bitSet(control, 4);
+    if (getBit(super_command, 1)) bitSet(control, 5);
+  }
+ 
+  sendByte(control);
+  crc.add(control);
+}
+
+void sendControlInfo(bool pool_or_final = true){ 
+  sendControl(info, send_seq_num, pool_or_final, recv_seq_num);
+}
+
+void sendControlRR(bool final = true){ 
+  sendControl(super, send_seq_num, final, recv_seq_num, 0b00); // RR
+}
+
+void sendControlREJ(bool final = true){ 
+  sendControl(super, send_seq_num, final, recv_seq_num, 0b01); // REJ
+}
+
+void sendPayload(uint16_t payload_size = auto_payload_size, uint16_t init_index = 0){
+  // Big Endian: send first the most significant byte in payload index range
+  if (not payload_size or init_index > MAX_PAYLOAD_SIZE-1) {
+    sendByte(0); // send at least an empty byte, once is an information frame
+    crc.add(0);
+  }
+  else { // general case
+    for (uint16_t index = init_index;
+        index < (payload_size > MAX_PAYLOAD_SIZE ? MAX_PAYLOAD_SIZE : payload_size); // index overflow handler
+        index++) {  // swapped input handler 
+      sendByte(payload_[index]);
+      crc.add(payload_[index]);
+    }
+  }
+}
+
+void addToPayloadBuffer(String input_, bool reset = true, uint16_t init_index = 0, bool set_payload_size = true){
+  if (reset) memset(payload_, 0, MAX_PAYLOAD_SIZE * sizeof(char)); // reset payload buffer
+  if (set_payload_size) auto_payload_size = (input_.length() < MAX_PAYLOAD_SIZE ? input_.length() : MAX_PAYLOAD_SIZE);
+  if (init_index < MAX_PAYLOAD_SIZE) { // index overflow handler
+    for (uint16_t index = init_index;
+         index < (sizeof(input_) > MAX_PAYLOAD_SIZE-init_index ? MAX_PAYLOAD_SIZE : input_.length());
+         index++) {  
+      payload_[index] = input_[index];
+    } 
+  }
+}
+
+void sendCRC(){
+  // cyclic redundancy check (CRC) is an error-detecting code
+  uint16_t crc_result = crc.calc();
+  sendByte(crc_result>>8);
+  sendByte((crc_result<<8)>>8);
+}
+
+void sendInfoFrame(uint8_t forward_address, bool pool_or_final = true, bool reset_sequence = true, bool increment_sequence = true){
+  if (reset_sequence)     send_seq_num = 1; else
+  if (increment_sequence) send_seq_num++;
+  if (DEBUG_LEVEL > 0) Serial.print("\nSEND:");
+  sendFlag();
+  sendAddress(forward_address);
+  sendControlInfo();
+  sendPayload();
+  sendCRC();
+  sendFlag(true);
+  if (DEBUG_LEVEL > 0) Serial.print((String)" n(s)="+send_seq_num+" n(r)="+recv_seq_num+" ASCII:'"+payload_+"' P/F:"+pool_or_final+" (Info)");
+}
+
+void sendSuperFrame(uint8_t forward_address, uint8_t command = 0b00, bool pool_or_final = true){
+  if (DEBUG_LEVEL > 0) Serial.print("\nSEND:");
+  sendFlag();
+  sendAddress(forward_address);
+  if (command == 0b00) sendControlRR(pool_or_final); else sendControlREJ(pool_or_final);
+  sendCRC();
+  sendFlag(true);
+  if (DEBUG_LEVEL > 0) Serial.print((String)" n(r)="+recv_seq_num+" P/F:"+pool_or_final+" (Super - "+(command == 0b00 ? "RR)" : (command == 0b01 ? "REJ)" : "ERR)")));
+}
+
+uint8_t processFrame(bool reset_recv_seq_num = true){
+  // - 0: None
+  // - 1: INFO (CRC OK)
+  // - 2: INFO (CRC REJ)
+  // - 3: SUPER RR  (CRC OK)
+  // - 4: SUPER RR  (CRC REJ)
+  // - 5: SUPER REJ (CRC OK)
+  // - 6: SUPER REJ (CRC REJ) 
+  // - 7: ERROR
+
+  if (reset_recv_seq_num) recv_seq_num = 1; // reset expected counter
+  if (not frame.ready) return 0;
+
+  bool pool_or_final = getBit(frame.buffer[2], 3);
+  
+  delay(250);
+  if (DEBUG_LEVEL > 0) { // print received bytes
+    Serial.print("\nRECV:");
+    for (uint8_t index = 0; index < frame.lenght; index++) {
+      Serial.print(frame.buffer[index] > 0xf ? " 0x" : " 0x0"); 
+      Serial.print(frame.buffer[index], HEX);
+    }
+  }
+
+  if (frame.buffer[1] != LOCAL_ADDRESS) {
+    if (DEBUG_LEVEL > 0) Serial.print(" <drop>"); 
+    resetFrameBuffer();
+    return 9; // drop frame cause different address
+  }
+
+  if (not getBit(frame.buffer[2], 7)){ // Information frame process
+    char payload_[MAX_PAYLOAD_SIZE];
+    memset(payload_, 0, MAX_PAYLOAD_SIZE * sizeof(char)); // clean payload buffer
+
+    // extract payload
+    for (uint8_t index = 0; index < frame.lenght - 6; index++) payload_[index] = frame.buffer[index+3];
+    
+    // test crc
+    crc.restart();
+    for (uint8_t index = 0; index < frame.lenght - 4; index++) crc.add(frame.buffer[index+1]);
+    uint16_t crc_result = crc.calc(); 
+
+    bool crc_ok = false;
+    if (crc_result>>8     == frame.buffer[frame.lenght-3] and 
+       (crc_result<<8)>>8 == frame.buffer[frame.lenght-2]) crc_ok = true; // TEST / SIMULATE CRC FAIL HERE
+    
+    crc.restart();
+
+    if (DEBUG_LEVEL > 0) Serial.print((String)" n(s)="+send_seq_num+" n(r)="+recv_seq_num+" ASCII:'"+payload_+"' CRC:"+(crc_ok ? "OK" : "REJ")+" P/F:"+pool_or_final+" (Info)");
+
+    Serial.print((String)"\n>> recv:'"+(String)payload_+"' CRC:"+(crc_ok ? "OK" : "REJ")+" n(s)="+send_seq_num+" n(r)="+recv_seq_num+" from"); 
+    Serial.print(linked_device > 0xf ? " 0x" : " 0x0"); Serial.print(linked_device, HEX);
+    Serial.print((String)"\n>> resp: "+(crc_ok ? "RR" : "REJ")+" n(r)="+recv_seq_num+" to"); 
+    Serial.print(linked_device > 0xf ? " 0x" : " 0x0"); Serial.print(linked_device, HEX);
+    
+    sendSuperFrame(linked_device, (crc_ok ? 0b00 : 0b01), not crc_ok); // {ADDRESS, SUPERVISORY CODE - 0b00(RR) or 0b01(REJ), POOL or FINAL} 
+    resetFrameBuffer();
+
+    if ((String)payload_ == "route Heyyy! to 0x03" and crc_ok){
+      String message = "Heyyy!";
+      linked_device = 0x03;
+      Serial.print((String)"\n>> send:'"+message+"' to 0x03");
+      addToPayloadBuffer(message);
+      delay(100);
+      return 8; // Response READY 
+    }
+
+    if ((String)payload_ == "route hELLooo! to 0x02" and crc_ok){
+      String message = "hELLooo!";
+      linked_device = 0x02;
+      Serial.print((String)"\n>> send:'"+message+"' to 0x02");
+      addToPayloadBuffer(message);
+      delay(100);
+      return 8; // Response READY 
+    }
+
+    if ((String)payload_ == "route Byyye! to 0x03" and crc_ok){
+      String message = "Byyye!";
+      linked_device = 0x03;
+      Serial.print((String)"\n>> send:'"+message+"' to 0x03");
+      addToPayloadBuffer(message);
+      delay(100);
+      return 8; // Response READY 
+    }
+
+    if ((String)payload_ == "route See u! to 0x02" and crc_ok){
+      String message = "See u!";
+      linked_device = 0x02;
+      Serial.print((String)"\n>> send:'"+message+"' to 0x02");
+      addToPayloadBuffer(message);
+      delay(100);
+      return 8; // Response READY 
+    }
+
+    if (crc_ok) {
+      recv_seq_num++;
+      return 1; // INFO (CRC Ok)
+    }
+    return 2;   // INFO (CRC REJ)
+  }
+
+  if (getBit(frame.buffer[2], 7) and not getBit(frame.buffer[2], 6)){ // Supervisory frame process
+    uint8_t command = 0b00;
+    if (getBit(frame.buffer[2], 4)) bitSet(command, 0);
+    if (getBit(frame.buffer[2], 5)) bitSet(command, 1);
+
+    // test crc
+    crc.restart();
+    for (uint8_t index = 0; index < frame.lenght - 4; index++) crc.add(frame.buffer[index+1]);
+    uint16_t crc_result = crc.calc(); 
+
+    bool crc_ok = false;
+    if (crc_result>>8     == frame.buffer[frame.lenght-3] and 
+       (crc_result<<8)>>8 == frame.buffer[frame.lenght-2]) crc_ok = true;
+    
+    crc.restart();
+
+    if (DEBUG_LEVEL > 0) Serial.print((String)" n(r)="+recv_seq_num+" CRC:"+(crc_ok ? "OK" : "REJ")+" P/F:"+pool_or_final+" (Super - "+(command == 0b00 ? "RR)" : (command == 0b01 ? "REJ)" : "ERR)")));
+  
+    Serial.print((String)"\n>> recv: "+(command == 0b00 ? "RR": "REJ")+" CRC:"+(crc_ok ? "OK" : "REJ")+" n(r)="+recv_seq_num+" from"); 
+    Serial.print(linked_device > 0xf ? " 0x" : " 0x0"); Serial.print(linked_device, HEX);
+
+    resetFrameBuffer();
+
+    if (command == 0b00 and crc_ok)     return 3; // SUPER RR  (CRC OK)
+    if (command == 0b00 and not crc_ok) return 4; // SUPER RR  (CRC REJ)
+    if (command == 0b01 and crc_ok)     return 5; // SUPER REJ (CRC OK)
+    if (command == 0b01 and not crc_ok) return 6; // SUPER REJ (CRC REJ)
+    return 7;   // INFO (CRC REJ)
+  }
+}
+
+bool sendMessage(uint8_t forward_address, uint16_t timeout_ms = 3000, uint16_t retry_delay_ms = 500, int retry_times = 4){ 
+  // This function handles the transmission of messages, 
+  // performing loop attempts according to the input parameters, 
+  // if it receives REJ or reaches the timeout
+  // * negative retry_times mean non stop loop
+  
+  uint8_t  output_code = 0;
+  unsigned long init_time = 0;
+
+  while (output_code != 3){
+    if (not init_time) sendInfoFrame(forward_address); // first emission
+    else {
+      if (retry_times > 0) retry_times --; else
+      if (not retry_times) break; // cancel
+      delay(retry_delay_ms);
+      sendInfoFrame(forward_address, true, false, false); // retry
+    }
+    output_code = 0;
+    init_time = millis();
+    while (millis() < init_time + timeout_ms){
+      scanByte();
+      if (received_byte.is_refreshed) addToFrameBuffer(received_byte.value);
+      if (frame.ready) {
+        output_code = processFrame();
+        if (output_code == 3) return true; // SUPER RR  (CRC OK)
+        break;
+      }
+    }
+    if (DEBUG_LEVEL > 0) Serial.print((output_code ? " <REJ/ERR>" : " <timeout>")); 
+
+  }
+  if (DEBUG_LEVEL > 0) Serial.print(" <canceled>"); 
+  return false;
+}
+
+// ******************************************************************
+
+// ##########################
+// ##### MAIN FUNCTIONS #####
+// ##########################
+void setup(){
+  Serial.begin(9600);
+  Serial.print((String)"\nPrimary Device - Initing...\nMy Address: "+LOCAL_ADDRESS);
+  
+  pinMode(A_PIN, INPUT); pinMode(B_PIN, INPUT_PULLUP);  // initial state is receive mode
+  memset(payload_, 0, MAX_PAYLOAD_SIZE * sizeof(char)); // clean payload buffer
+  resetFrameBuffer();
+  received_byte.is_refreshed = false;
+  received_byte.value = 0;
+  crc.restart();
+
+  send_seq_num = 1;
+  recv_seq_num = 1;
+}
+
+void loop(){
+  while (not Serial.available()){
+    scanByte();
+    if (received_byte.is_refreshed) addToFrameBuffer(received_byte.value);
+    if (frame.ready and processFrame() == 8) { // "8" code is Response READY
+      delay(500); // delay to give time to notice as different serial print outputs, or different frames in the osciloscope
+      sendMessage(linked_device);
+      resetFrameBuffer();
+    }
+  } 
+
+  String message = Serial.readString();
+  if (message.length() > 0){
+    if (message[message.length()-1] == 0x0d) message.remove(message.length()-1); // remove CR
+    if (not message.length()) return; 
+    Serial.print((String)"\n>> send:'"+message+"' to");
+    while (not Serial.available()) delay(10);
+    uint8_t forward_address = (uint8_t)Serial.parseInt();
+    Serial.print(forward_address > 0xf ? " 0x" : " 0x0"); Serial.print(forward_address, HEX);
+
+    addToPayloadBuffer(message);
+    sendMessage(forward_address);
+  }
+
+}
+// ******************************************************************
+
+// ##### SUPPORT FUNCTIONS #####
+bool getBit(unsigned char byte, int position){
+  return (byte >> position) & 0x1; // position 0-7
+}
